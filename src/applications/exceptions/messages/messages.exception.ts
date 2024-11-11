1914b8520973cdf3df3a33966efe424e4afa9d4b
guyzoum77 <guyzoum7700@mail.com>
2024-11-11 17:55:06 +0000

    new architecture

--- src/applications/exceptions/messages/messages.exception.ts
+++
@@ -1,7 +0,0 @@
-/**
- * write your own exception message like this : static mySQLError: string = "MysqlError";
- */
-
-export const messagesException = {
-    //userNotFound: "A user is not found"
-}
